209b26927f85bf203beb8d3874bf354ee4d9ca8f
ntub03902027 <b03902027@ntu.edu.tw>
2017-06-25 17:59:34 +0800

    revise HSV formula, still need testing

--- PixelSortGPU.cu
+++ PixelSortGPU.cu
@@ -9,22 +9,27 @@
 
 __device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
 __device__ __host__ int CeilAlign(int a, int b) { return CeilDiv(a, b) * b; }
-__device__ __host__ float MaxRGB(const float R, const float G, const float B) {
+__device__ __host__ __forceinline__ float MaxRGB(const float R, const float G, const float B) {
     if(R > G && R > B) return R;
     else if (G > R && G > B) return G;
     else if (B > R && B > G) return B;
     else return R; // R == G && R == B
 }
-__device__ __host__ float MinRGB(const float R, const float G, const float B) {
+__device__ __host__ __forceinline__ float MinRGB(const float R, const float G, const float B) {
     if(R < G && R < B) return R;
     else if (G < R && G < B) return G;
     else if (B < R && B < G) return B;
     else return R; // R == G && R == B
 }
-__device__ __host__ float absolute(float x){
+__device__ __host__ __forceinline__ float absolute(float x){
     if (x >= 0)
         return x;
     return -x;
+}
+__device__ __host__ __forceinline__ float angle_nomralize(float x){
+    if (x < 0.0)
+        return x + 360.0;
+    return x;
 }
 __device__ __host__ float getLuminance(const float R, const float G, const float B) {
     float R_ = R/255.0f;
@@ -32,7 +37,7 @@ __device__ __host__ float getLuminance(const float R, const float G, const float
     float B_ = B/255.0f;
     float M = MaxRGB(R_, G_, B_);
     float m = MinRGB(R_, G_, B_);
-    return (M + m) / 2;
+    return (M + m) / 2.0;
 }  
 __device__ __host__ float getHue(const float R, const float G, const float B) {
     float M = MaxRGB(R, G, B);
@@ -43,34 +48,27 @@ __device__ __host__ float getHue(const float R, const float G, const float B) {
     if (C > -0.1f && C < 0.1f) //C == 0.0f
         Result = 0.0f;
     else if (M == R)
-        Result = fmodf((G - B) / C, 6);
+        Result = (G - B) / C;
     else if (M == G)
         Result = 2.0f + ( (B - R) / C );
     else if (M == B)
         Result = 4.0f + ( (R - G) / C );
-    return 60.0f*Result;
+    return angle_nomralize(60.0f*Result);
 }
 __device__ __host__ float getSaturation(const float R, const float G, const float B) {
-    float R_ = R/255.0f;
-    float G_ = G/255.0f;
-    float B_ = B/255.0f;
-
-    float M = MaxRGB(R_, G_, B_);
-    float m = MinRGB(R_, G_, B_);
+    float M = MaxRGB(R, G, B) / 255.0f;
+    float m = MinRGB(R, G, B) / 255.0f;
     float C = M - m;
-    float L = (M + m) / 2;
+    float L = (M + m) / 2.0;
 
-    if (L == 1)
-        return 0;
-    else
-        return C / (1 - absolute(2 * L - 1));
+    return (L != 1.0)? C / (1 - absolute(2 * L - 1)) : 0.0;
 }
 // For the following 3 functions, adapted from: https://www.jiuzhang.com/solutions/kth-largest-element/
 __device__ __host__ int kthLargestPartition(int l, int r, Pixel pixel_array[]) {
 
     int left = l, right = r;
     Pixel temp = pixel_array[left];
-    int pivot = temp.key;
+    float pivot = temp.key;
        
 
     while (left < right) {
@@ -114,7 +112,7 @@ __device__ __host__ int kthLargest(int k, int length, Pixel pixel_array[]) {
 #define debug_print(...)
 #endif
 
-#define OUPUT_POINT_MAX 100
+#define OUPUT_POINT_MAX 5000
 
 //#define PREDEBUG
 
@@ -140,7 +138,7 @@ __device__  void GetListToSort(
     while (cnt < OUPUT_POINT_MAX && 
            last[0] > 0 && last[0] < w && 
            last[1] > 0 && last[1] < h &&
-           PIXELXY(last[0], last[1]).key > 0.0f) {
+           PIXELXY(last[0], last[1]).key >= 0.0f) {
         // TODO: AA here
         output[cnt] = PIXELXY(last[0], last[1]);
         ++cnt;
@@ -156,7 +154,7 @@ __device__  void GetListToSort(
     while (cnt < OUPUT_POINT_MAX && 
            last[0] > 0 && last[0] < w && 
            last[1] > 0 && last[1] < h &&
-           PIXELXY(last[0], last[1]).key > 0.0f) {
+           PIXELXY(last[0], last[1]).key >= 0.0f) {
         // TODO: AA here
         output[cnt] = PIXELXY(last[0], last[1]);
         ++cnt;
@@ -250,6 +248,13 @@ __global__ void ComputeKey(
                 break;
         }
         cur->key = Map01(cur->key, min, max, threshold_min, threshold_max);
+        /*
+        if (cur->key > 0.0f) {
+            cur->r = 255.0f;
+            cur->g /= 2;
+            cur->b /= 2;
+        }
+        */
     }
 }
 
@@ -269,13 +274,27 @@ __global__ void SortFromList(PixelSortPatternParmLinear *linear,
 
         int point_cnt_gpu;
         int order_gpu;
-        Pixel pixel_list_gpu[OUPUT_POINT_MAX];
 
+        Pixel pixel_list_gpu[OUPUT_POINT_MAX];
         // Get a list for sorting
         GetListToSort(input, linear, pixelx, pixely, (float)w, (float)h, &order_gpu, &point_cnt_gpu, pixel_list_gpu);
 
         // Sorting
+
+#if 1
         int search_index = kthLargest(point_cnt_gpu - order_gpu, point_cnt_gpu, pixel_list_gpu);
+#else
+        for (int i = 0; i < point_cnt_gpu; i++) {
+            for (int j = 0; j < point_cnt_gpu - i; j++){
+                if (pixel_list_gpu[j].key > pixel_list_gpu[j+1].key) {
+                    Pixel temp = pixel_list_gpu[j];
+                    pixel_list_gpu[j] = pixel_list_gpu[j+1];
+                    pixel_list_gpu[j+1] = temp;
+                }
+            }
+        }
+        int search_index = order_gpu;
+#endif
 
         // Fill value: order
         output[pixelid].r = pixel_list_gpu[search_index].r;
@@ -390,6 +409,8 @@ void PixelSortGPU(Pixel *input, int width, int height, Pixel *output,
         default:
             break;
 	}
+
+    //cudaMemcpy(output, input, width*height*sizeof(Pixel), cudaMemcpyDeviceToDevice);
 }
 
 
